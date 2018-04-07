3f819c5b30d96d80c5fdfff46e2b0b43a17d3617
Konrad Gądek <kgadek@gmail.com>
2018-04-08 01:00:14 +0200

    feature:API-PR activity: data structures & deserialization (w/ tests)

---
+++ src/bb_api/repositories/username/repo_slug/pullrequests/pull_request_id/activity.rs
@@ -0,0 +1,44 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use serde_json::value::Value;
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct Activity {
+    pub pagelen: u32,
+    pub next: Option<String>,
+    pub values: Vec<ActivityItem>,
+}
+
+#[derive(PartialEq, Debug, Deserialize)]
+#[serde(untagged)]
+pub enum ActivityItem {
+    Comment { comment: Value },
+    Update { update: Value },
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct Comment {
+    pub id: u32,
+    pub parent: Option<CommentParent>,
+    pub content: CommentContent,
+    pub user: CommentUser,
+    pub created_on: String,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct CommentParent {
+    pub id: u32,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct CommentContent {
+    pub raw: String,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct CommentUser {
+    pub username: String,
+    pub display_name: String,
+}
--- src/bb_api/repositories/username/repo_slug/pullrequests/pull_request_id/mod.rs
+++ src/bb_api/repositories/username/repo_slug/pullrequests/pull_request_id/mod.rs
@@ -2,11 +2,4 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-
-mod bb_api;
-#[cfg(test)]
-mod tests;
+pub mod activity;
