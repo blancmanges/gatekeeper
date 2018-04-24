88709add28a06ac7097a7d08b56748dafc13184d
Konrad Gądek <kgadek@gmail.com>
2018-04-25 00:27:12 +0200

    refactor: big cleanup
    During some discussions today, @amnk pointed out that bors [1] is merely
    ~900 LOC Python script. This led me to notice *lots* of unnecessary code
    (I didn't apply [YAGNI] principle enough).
    
    With all of this in mind, I've refactored the project a tiny bit /s
    
    1. Tests. I've removed them, as most of them made little sense. Example:
       tests in `bb_api::(..)::pullrequests::pull_request_id::activity` were
       there to tell if example response is deserializable -- with intention
       to prevent accidental breakage of serde-derived data structures.  But
       they imposed some additional burden that didn't seem to pay off. Only
       root tests made some sense, as they tested parsing logic. However, it
       seems pretty premature as we don't have any command spec (yet). It is
       not a problem to restore them in future too.
    
    2. Module tree. Fully flattened. The intent was to cover whole BitBucket
       API with it (and probably separate a crate).  That doesn't seem to be
       needed for Gatekeeper, as few data structures are enough.
    
    3. CLI option parsing.  Clap is perfectly fine, however use of structopt
       allowed to reduce number of lines.  Tradeoff:  inability to read from
       environment variables.
    
    4. Small code tweaks here and there. For example I've moved `unpaginate`
       to `impl <T> Paginated<T>`,  added some logic into `UserCommand::new`
       so that it strips `\\` from the beginning of the string,  made helper
       data structures (`PullrequestsURLs` and `PullrequestIdURLs`) in order
       to clarify some argument passing, and created `BitBucketApiBasicAuth`
       which replaces closure (and should allow bit more composability).
    
    5. Dropping cargo workspace. This comes from the simplification: no need
       for full-crate with BitBucket API, no need for parsing crate, etc.
    
    [1]: https://github.com/graydon/bors
    [YAGNI]: https://en.wikipedia.org/wiki/You_aren%27t_gonna_need_it

---
+++ src/bitbucket.rs
@@ -0,0 +1,163 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use std::fmt::Debug;
+
+use failure::Error;
+use reqwest;
+use serde;
+use serde_json;
+use slog;
+
+pub struct BitBucketApiBasicAuth {
+    client: reqwest::Client,
+    username: String,
+    password: String,
+}
+
+impl BitBucketApiBasicAuth {
+    pub fn new(username: String, password: String) -> BitBucketApiBasicAuth {
+        BitBucketApiBasicAuth {
+            client: reqwest::Client::new(),
+            username,
+            password,
+        }
+    }
+    pub fn get_json(&self, url: &str) -> reqwest::Result<reqwest::Response> {
+        let mut req_builder = self.client.get(url);
+        req_builder
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .header(reqwest::header::ContentType::json());
+        req_builder.send()
+    }
+}
+
+#[derive(Deserialize, PartialEq, Debug, Clone)]
+pub struct Paginated<T> {
+    pub values: Vec<T>,
+    pub next: Option<String>,
+}
+
+impl<T> Paginated<T>
+where
+    T: serde::de::DeserializeOwned + Debug,
+{
+    pub fn unpaginate(
+        self,
+        client: &BitBucketApiBasicAuth,
+        logger: &slog::Logger,
+    ) -> Result<Vec<T>, Error> {
+        let mut current = self;
+
+        let mut res: Vec<T> = Vec::new();
+        res.append(&mut current.values);
+
+        while let Some(next_url) = current.next {
+            debug!(logger, "Requesting next page: {}", next_url);
+            let mut result = client.get_json(next_url.as_str())?;
+            trace!(logger, "Response: {:?}", result);
+            let res_txt = result.text()?;
+
+            current = serde_json::from_str(res_txt.as_str())?;
+
+            trace!(logger, "Response: {:?}", current);
+            res.append(&mut current.values);
+        }
+
+        Ok(res)
+    }
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct PullRequest {
+    pub id: u32,
+    pub title: String,
+    pub state: String,
+    pub links: PullRequestLinks,
+    pub author: PullRequestUser,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct PullRequestLinks {
+    #[serde(rename = "self")]
+    pub slf: Href,
+    pub activity: Href,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct Href {
+    pub href: String,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct PullRequestUser {
+    pub username: String,
+}
+
+type Ignored = serde_json::value::Value;
+
+#[derive(PartialEq, Debug, Deserialize)]
+#[serde(untagged)]
+pub enum ActivityItem {
+    Comment { comment: Comment },
+    Update { update: Ignored },
+    Approval { approval: Approval },
+}
+
+#[derive(Deserialize, PartialEq, Debug, Clone)]
+pub struct Comment {
+    pub id: u32,
+    pub parent: Option<CommentParent>,
+    pub content: Content,
+    pub user: User,
+}
+
+impl Comment {
+    pub fn is_top_level(&self) -> bool {
+        self.parent == None
+    }
+}
+
+#[derive(Deserialize, PartialEq, Debug, Clone)]
+pub struct CommentParent {
+    pub id: u32,
+}
+
+#[derive(Deserialize, PartialEq, Debug, Clone)]
+pub struct Content {
+    pub raw: String,
+}
+
+#[derive(Deserialize, PartialEq, Debug, Clone)]
+pub struct Approval {
+    pub user: User,
+}
+
+#[derive(Deserialize, PartialEq, Debug, Clone)]
+pub struct User {
+    pub username: String,
+}
+
+pub fn get_all<T>(
+    url: &str,
+    client: &BitBucketApiBasicAuth,
+    logger: &slog::Logger,
+) -> Result<Vec<T>, Error>
+where
+    T: serde::de::DeserializeOwned + Debug,
+{
+    let logger = logger.new(o!(
+        "url" => url.to_string(),
+    ));
+
+    trace!(logger, "Obtaining first page");
+    let mut first_page = client.get_json(url)?;
+    let first_page = first_page.text()?;
+    trace!(logger, "Response text: {}", first_page);
+    trace!(logger, "Deserializing");
+    let first_page: Paginated<T> = serde_json::from_str(first_page.as_str())?;
+
+    trace!(logger, "Getting remaining pages");
+    first_page.unpaginate(&client, &logger)
+}
---
+++ src/main.rs
@@ -0,0 +1,145 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+extern crate failure;
+extern crate gatekeeper;
+extern crate reqwest;
+extern crate serde_json;
+#[macro_use]
+extern crate slog;
+extern crate sloggers;
+#[macro_use]
+extern crate structopt;
+
+use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
+
+use gatekeeper::bitbucket::get_all;
+use gatekeeper::bitbucket::BitBucketApiBasicAuth;
+use gatekeeper::bitbucket::PullRequest;
+use gatekeeper::get_commands;
+use gatekeeper::PullrequestsURLs;
+
+use failure::Error;
+use sloggers::Build;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    #[structopt(short = "u", long = "bitbucket-username")]
+    bitbucket_username: String,
+    #[structopt(short = "p", long = "bitbucket-password")]
+    bitbucket_password: String,
+    #[structopt(short = "o", long = "bitbucket-repo-owner")]
+    repo_owner: String,
+    #[structopt(short = "r", long = "bitbucket-repo-slug")]
+    repo_slugs: Vec<String>,
+}
+
+fn app(logger: &slog::Logger) -> Result<(), Error> {
+    debug!(logger, "Starting application");
+    let app_args = Opt::from_args();
+    let client =
+        BitBucketApiBasicAuth::new(app_args.bitbucket_username, app_args.bitbucket_password);
+
+    debug!(logger, "Repositories: {:?}", app_args.repo_slugs);
+    for repo_slug in &app_args.repo_slugs {
+        repo_prs(&app_args.repo_owner, &repo_slug, &client, &logger)?;
+    }
+
+    Ok(())
+}
+
+fn repo_prs(
+    repo_owner: &str,
+    repo_slug: &str,
+    client: &BitBucketApiBasicAuth,
+    logger: &slog::Logger,
+) -> Result<(), Error> {
+    let logger = logger.new(o!(
+        "repo_owner" => repo_owner.to_string(),
+        "repo_slug" => repo_slug.to_string(),
+    ));
+
+    debug!(logger, "Processing repo");
+
+    println!("{}", repo_slug);
+    println!("------------------------------------------------------------------------");
+
+    let urls = PullrequestsURLs::new(repo_owner, repo_slug);
+
+    debug!(logger, "Obtaining BB/{{repo}}/pullrequests/");
+    let pullrequests = get_all(&urls.api_url, &client, &logger)?;
+    trace!(logger, "PRs: {:?}", pullrequests);
+
+    for pr in pullrequests {
+        repo_pr(pr, &urls, &client, &logger)?;
+    }
+
+    Ok(())
+}
+
+fn repo_pr(
+    pr: PullRequest,
+    urls: &PullrequestsURLs,
+    client: &BitBucketApiBasicAuth,
+    logger: &slog::Logger,
+) -> Result<(), Error> {
+    let logger = logger.new(o!(
+        "pr_id" => pr.id,
+    ));
+
+    let urls = urls.with_id(pr.id);
+
+    debug!(logger, "PR title: {}", pr.title);
+    trace!(logger, "PR: {:?}", pr);
+    trace!(logger, "Urls: {:?}", urls);
+
+    debug!(logger, "Obtaining BB/{{repo}}/pullrequests/{{id}}");
+    let activity = {
+        let mut activity = get_all(&pr.links.activity.href, &client, &logger)?;
+        activity.reverse();
+        activity
+    };
+    trace!(logger, "Activity: {:?}", activity);
+
+    let commands = get_commands(activity);
+    trace!(logger, "Commands: {:?}", commands);
+
+    // ---------------------------------------------------------------------------------------------
+
+    let mut review_status: HashMap<String, String, RandomState> = HashMap::new();
+    for user_command in commands {
+        match user_command.command.as_str() {
+            "+1" | "+0" | "-1" => {
+                let vote = review_status
+                    .entry(user_command.user.to_string())
+                    .or_insert(String::from("0"));
+                *vote = user_command.command;
+            }
+            _ => {}
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    println!("  PR {}: {}", pr.id, pr.title);
+    println!("    -- author: {}", pr.author.username);
+    println!("    -- link: {}", urls.web_url);
+    for (user, vote) in &review_status {
+        println!("    {}: {}", user, vote);
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let mut logger = sloggers::terminal::TerminalLoggerBuilder::new();
+    logger.level(sloggers::types::Severity::Trace);
+    logger.destination(sloggers::terminal::Destination::Stderr);
+    let logger = logger.build().unwrap();
+
+    app(&logger).unwrap();
+}
