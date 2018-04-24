39f58417e3473f437118eb35ad83e2c76f7bc383
Konrad Gądek <kgadek@gmail.com>
2018-04-25 00:27:12 +0200

    refactor: big cleanup
    During some discussions today, @amnk pointed out that bors [1] is merely
    ~900 LOC Python script. This led me to notice *lots* of unnecessary code
    (I didn't apply [YAGNI] principle enough).
    
    With all of this in mind, I've refactored the project a tiny bit /s
    
    1. Tests. I've removed them, as most of them made little sense. Example:
       tests in `bb_api::(..)::pullrequests::pull_request_id::activity` were
       there to tell if example response is deserializable -- with intention
       to prevent accidental breakage of serde-derived data structures.  But
       they imposed some additional burden that didn't seem to pay off. Only
       root tests made some sense, as they tested parsing logic. However, it
       seems pretty premature as we don't have any command spec (yet). It is
       not a problem to restore them in future too.
    
    2. Module tree. Fully flattened. The intent was to cover whole BitBucket
       API with it (and probably separate a crate).  That doesn't seem to be
       needed for Gatekeeper, as few data structures are enough.
    
    3. CLI option parsing.  Clap is perfectly fine, however use of structopt
       allowed to reduce number of lines.  Tradeoff:  inability to read from
       environment variables.
    
    4. Small code tweaks here and there. For example I've moved `unpaginate`
       to `impl <T> Paginated<T>`,  added some logic into `UserCommand::new`
       so that it strips `\\` from the beginning of the string,  made helper
       data structures (`PullrequestsURLs` and `PullrequestIdURLs`) in order
       to clarify some argument passing, and created `BitBucketApiBasicAuth`
       which replaces closure (and should allow bit more composability).
    
    5. Dropping cargo workspace. This comes from the simplification: no need
       for full-crate with BitBucket API, no need for parsing crate, etc.
    
    [1]: https://github.com/graydon/bors
    [YAGNI]: https://en.wikipedia.org/wiki/You_aren%27t_gonna_need_it

---
+++ src/lib.rs
@@ -0,0 +1,125 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+extern crate failure;
+extern crate reqwest;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+#[macro_use]
+extern crate slog;
+extern crate sloggers;
+
+pub mod bitbucket;
+
+use bitbucket::ActivityItem;
+use bitbucket::Approval;
+use bitbucket::Comment;
+use bitbucket::Paginated;
+
+#[derive(Debug)]
+pub struct PullrequestsURLs {
+    pub api_url: String,
+    pub web_url: String,
+}
+
+impl PullrequestsURLs {
+    pub fn new(repo_owner: &str, repo_slug: &str) -> PullrequestsURLs {
+        PullrequestsURLs {
+            api_url: format!(
+                "https://api.bitbucket.org/2.0/repositories/{}/{}/pullrequests",
+                repo_owner, repo_slug
+            ),
+            web_url: format!(
+                "https://bitbucket.org/{}/{}/pull-requests",
+                repo_owner, repo_slug
+            ),
+        }
+    }
+
+    pub fn with_id(&self, id: u32) -> PullrequestIdURLs {
+        PullrequestIdURLs::new(&self, id)
+    }
+}
+
+#[derive(Debug)]
+pub struct PullrequestIdURLs {
+    pub api_url: String,
+    pub web_url: String,
+}
+
+impl PullrequestIdURLs {
+    pub fn new(pullrequests_link: &PullrequestsURLs, id: u32) -> PullrequestIdURLs {
+        let api_url = format!("{}/{}", pullrequests_link.api_url, id);
+        let web_url = format!("{}/{}", pullrequests_link.web_url, id);
+        PullrequestIdURLs { api_url, web_url }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct UserCommand {
+    pub user: String,
+    pub command: String,
+}
+
+impl UserCommand {
+    pub fn new(user: &str, command: &str) -> UserCommand {
+        let command = if command.starts_with("\\+") {
+            command.trim_left_matches("\\")
+        } else {
+            command
+        };
+        UserCommand {
+            user: user.to_string(),
+            command: command.to_string(),
+        }
+    }
+}
+
+pub fn activities_to_items(act: Vec<Paginated<ActivityItem>>) -> Vec<ActivityItem> {
+    act.into_iter().flat_map(|item| item.values).collect()
+}
+
+pub fn toplevel_comments(act_items: Vec<ActivityItem>) -> Vec<Comment> {
+    act_items
+        .into_iter()
+        .filter_map(|item| match item {
+            ActivityItem::Comment { comment } => if comment.is_top_level() {
+                Some(comment)
+            } else {
+                None
+            },
+            _ => None,
+        })
+        .collect()
+}
+
+pub fn get_commands(act_items: Vec<ActivityItem>) -> Vec<UserCommand> {
+    let mut commands = Vec::new();
+
+    for mut activity in act_items {
+        match activity {
+            ActivityItem::Comment {
+                comment: comment @ Comment { parent: None, .. },
+            } => for comment_line in comment.content.raw.lines() {
+                let mut splitter = comment_line.split_whitespace();
+                if Some("!g") == splitter.next() {
+                    for command in splitter {
+                        commands.push(UserCommand::new(&comment.user.username, command))
+                    }
+                }
+            },
+            ActivityItem::Approval {
+                approval: Approval { user },
+            } => commands.push(UserCommand {
+                user: user.username.clone(),
+                command: "+1".to_string(),
+            }),
+            _ => {}
+        }
+    }
+
+    commands
+}
